485bae82c94ebfb3d85e7cf6756ca2d733811321
Pavlo <pkhasanov@gmail.com>
2023-10-05 21:04:08 +0300

    Add project files.

---
+++ HW6_C_Sharp/Magazine.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm6_overload_of_operators
+{
+    internal class Magazine 
+    {
+        private string _title;
+        private string _year;
+        private string _description;
+        private string _phone;
+        private string _email;
+        public int Employees { get; set; }
+        public Magazine(int employees)
+        {
+            Employees = employees;
+        }
+        public static Magazine operator +(Magazine m1, Magazine m2)
+        {
+            return new Magazine(m1.Employees + m2.Employees);
+        }
+        public static Magazine operator +(Magazine m1, int value)
+        {
+            return new Magazine(m1.Employees + value);
+        }
+        public static Magazine operator -(Magazine m1, Magazine m2)
+        {
+            return new Magazine(m1.Employees - m2.Employees);
+        }
+        public static Magazine operator -(Magazine m1, int value)
+        {
+            return new Magazine(m1.Employees - value);
+        }
+        public static bool operator ==(Magazine m1, Magazine m2)
+        {
+            return m1.Employees == m2.Employees;
+        }
+        public static bool operator !=(Magazine m1, Magazine m2)
+        {
+            return m1.Employees != m2.Employees;
+        }
+        public static bool operator <(Magazine m1, Magazine m2)
+        {
+            return m1.Employees < m2.Employees;
+        }
+        public static bool operator >(Magazine m1, Magazine m2)
+        {
+            return m1.Employees > m2.Employees;
+        }
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception($"Variable can't be empty");
+                }
+                _title = value;
+            }
+        }
+        public string Year
+        {
+            get => _year;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception($"Variable can't be empty");
+                }
+                _year = value;
+            }
+        }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception($"Variable can't be empty");
+                }
+                _description = value;
+            }
+        }
+        public string Phone
+        {
+            get => _phone;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception($"Variable can't be empty");
+                }
+                _phone = value;
+            }
+        }
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception($"Variable can't be empty");
+                }
+                _email = value;
+            }
+        }
+        public override string ToString()
+        {
+            var input = new StringBuilder();
+
+            input.AppendLine($"Numer of Employees: {Employees}");
+
+            return input.ToString();
+        }
+    }
+}
---
+++ HW6_C_Sharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm6_overload_of_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Magazine m1 = new Magazine(20);
+            Magazine m2 = new Magazine(30);
+
+            m1 += m2;
+            Console.WriteLine(m1);
+
+            m2 += 10;
+            Console.WriteLine(m2);
+
+            m1 -= m2;
+            Console.WriteLine(m1);
+
+            m2 -= 10;
+            Console.WriteLine(m2);
+
+            if (m1 == m2)
+            {
+                Console.WriteLine($"{m1} == {m2}");
+            }
+            else
+            {
+                Console.WriteLine($"{m1} != {m2}");
+            }
+
+            if (m1 < m2)
+            {
+                Console.WriteLine($"{m1} < {m2}");
+            }
+            else
+            {
+                Console.WriteLine($"{m1} > {m2}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
